d53161fdaed818df2917c2eee8efd4de77f5c7ed
Andy Galasso <agalasso@bento.net>
2023-02-07 18:07:54 -0500

    Remove reference to DEBUG environment variable
    The DEBUG environment variable is used to initialize the _IS_DEBUG
    global variable which is not actually used.  If the environment
    happens to contain a DEBUG value that cannot be converted to int,
    the module throws a fatal ValueError and fails to load.

--- inotify/adapters.py
+++ inotify/adapters.py
@@ -34,7 +34,6 @@ _INOTIFY_EVENT = collections.namedtuple(
                     ])
 
 _STRUCT_HEADER_LENGTH = struct.calcsize(_HEADER_STRUCT_FORMAT)
-_IS_DEBUG = bool(int(os.environ.get('DEBUG', '0')))
 
 
 class EventTimeoutException(Exception):
